2d91c0f51365922cdf5ee1fd4de02ee19d38e261
Kanon Kakuno <yadex205@outlook.jp>
2017-11-16 18:27:32 +0900

    :+1: add hook to DOMContentLoaded

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
-global['sketches'] = {
-  title: new p5(require('./sketch/title'))
-}
+document.addEventListener('DOMContentLoaded', function() {
+  global['sketches'] = {
+    title: new p5(require('./sketch/title'))
+  }
+})
